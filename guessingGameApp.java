9339d4e407a509fa95a6ef2db6cf9c8926f72787
Unsaint <unsaintedwarrior@gmail.com>
2023-10-10 19:52:04 -0300

    Working code
    It works!
    
    Now more compact and with some more comments (not all of them!)

--- guessingGameApp.java
+++ guessingGameApp.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
-import guessing.guessingGameLogic;
+// Import
+import java.util.Scanner; // Import Scanner utility
+import ignore.guessingGameLogic; // Import .java file from the guessing package
 
+
+// Code
 public class guessingGameApp {
-    private Scanner scanner = new Scanner(System.in);
-
+    private Scanner scanner = new Scanner(System.in); // Create a Scanner object to collect user input
     public static void main(String[] args) {
         System.out.println("Qual sua idade?");
-        
-        int userAge = new guessingGameApp().scanner.nextInt(); // Read user input using the shared scanner
 
-        if (userAge < 12) {
-            guessingGameLogic gameLogic = new guessingGameLogic();
-            gameLogic.playYoungMode();
+        int userAge = new guessingGameApp().scanner.nextInt(); // Read user input
+
+        System.out.println("Your age is: " + userAge); // Display the user's age for verification
+
+        guessingGameLogic gameLogic; // Declare a variable to hold guessingGameLogic connection
+
+        if (userAge < 12) { // Check the user's age and select the appropriate game version
+            gameLogic = new guessingGameLogic(5, 10); // Set the "young" version with the propper parameters
+        } else {
+            // (guessingGameLogic) //Activate (>= 12) range
+            gameLogic = new guessingGameLogic(10, 1000); // Set the "old" version with the propper parameters
         }
 
-        if (userAge >= 12) {
-            // Handle adult mode if needed
-        }
+        gameLogic.playGame(); // Call "playGame" method from guessing.guessingGameLogic
+        gameLogic.closeScanner(); // Clean up: close the scanner object
     }
 }
-
